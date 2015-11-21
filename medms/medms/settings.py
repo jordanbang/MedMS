fa18c5f9bf8e441484e25f4e855f0935aec2979e
Jordan Bangia <george.doe231@gmail.com>
2015-11-21 13:17:09 -0500

    Setup for heroku

--- medms/medms/settings.py
+++ medms/medms/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'medms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':    'mysql',  #'heroku_17d2be463a47115',
-        'USERNAME': 'jordanbangia',                             #'b90674e75b96be',
-        'PASSWORD': 'password',                                 #'0c30a494',
-        'HOST': 'localhost',                            #'us-cdbr-iron-east-03.cleardb.net',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':    'mysql',  #'heroku_17d2be463a47115',
+#         'USERNAME': 'jordanbangia',                             #'b90674e75b96be',
+#         'PASSWORD': 'password',                                 #'0c30a494',
+#         'HOST': 'localhost',                            #'us-cdbr-iron-east-03.cleardb.net',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Internationalization
@@ -105,3 +105,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
