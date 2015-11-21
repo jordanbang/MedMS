5da18692820ea48204c05ba21b324a789308e396
Jordan Bangia <george.doe231@gmail.com>
2015-11-21 13:52:19 -0500

    Fixed weird file structure

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Doctor, Availability
+from app.models import Doctor, Availability
 
 admin.site.register(Doctor)
 admin.site.register(Availability)
--- medms/settings.py
+++ medms/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'medms.urls'
+ROOT_URLCONF = 'a.urls'
 
 TEMPLATES = [
     {
