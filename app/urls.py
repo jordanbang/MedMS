d5a2dc8f431afa0e8e0d257555c141f4717b41fb
Sagun B <sagun.b@gmail.com>
2015-11-21 15:23:46 -0500

    sms endpoint

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from app import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^message/newsms/$', views.receive_sms, name='smsResponder')
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from sms_service import SmsService
 
+sms_sender = SmsService()
 # Create your views here.
 
 def index(request):
     return HttpResponse("What up?")
+
+
+@require_http_methods(['POST'])
+def receive_sms(request):
+    """Respond to a new sms"""
+
+    from_number = request.POST.get('From')
+    body = request.POST.get('Body')
+
+    resp = sms_sender.reply_to_message()
+    sos = "Dear MedMS volunteers. A patient with the following number: {} is requesting assistance. " \
+          "Patient's message: {}".format(from_number, body)
+    failed_messages = sms_sender.send_new_message(sos, get_available_doctors())
+
+    return str(resp)
+
+
+def get_available_doctors():
+    return ['4169488810']
