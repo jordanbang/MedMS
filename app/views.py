baa590f554449ed7e4827f812b292c26eb6d149d
Sagun B <sagun.b@gmail.com>
2015-11-21 20:27:29 -0500

    try to save data to db when patient requests for help

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
-from models import Availability
+from models import Availability, PatientRequests
 from sms_service import SmsService
 
 from django.contrib.auth.models import User
@@ -34,6 +34,10 @@ def receive_sms(request):
             body = request.POST.get('Body')
 
         resp = sms_sender.reply_to_message()
+        location = location_extractor(body)
+
+        PatientRequests(patient=from_number, location=location, open=True).save()
+
         sos = "Dear MedMS volunteers. A patient with the following number: {} is requesting assistance. " \
               "Patient's message: {}".format(from_number, body)
         failed_messages = sms_sender.send_new_message(sos, get_available_doctors())
@@ -42,6 +46,16 @@ def receive_sms(request):
 
     else:
         return HttpResponse(str(get_available_doctors()))
+
+
+def location_extractor(msg):
+    words = msg.lower().split(' ')
+    try:
+        loc = words.index('location')
+        location = words[loc + 1]
+    except:
+        location = 'Unspecified'
+    return location.upper()
 
 
 def get_available_doctors():
