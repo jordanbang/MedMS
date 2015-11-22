b413b96e1c4406b666357155f2a45171bbd53a94
Sagun B <sagun.b@gmail.com>
2015-11-21 20:20:02 -0500

    register open requests table in admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Doctor, Availability
+from app.models import Doctor, Availability, PatientRequests
 
 admin.site.register(Doctor)
 admin.site.register(Availability)
+admin.site.register(PatientRequests)
